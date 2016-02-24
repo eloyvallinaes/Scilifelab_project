0f9f48357476d38ffdb4f6cef842fb25bcd3db05
Eloy <eloyvallina33@gmail.com>
2016-02-24 20:48:17 +0100

    Finally working

--- scripts/set-divide.py
+++ scripts/set-divide.py
@@ -7,32 +7,35 @@
 # 5 fold cross validation scheme
 # Argument 1: keyname
 # Agumentt 2: data set selection
-	# 1 2 3 4 5
+	# 0 1 2 3 4
 # Dataset already parsed to svm-light format
 import sys
 
 i=int(sys.argv[2])
-j=range(1, 6)
+j=range(0, 5)
 j.remove(i)
 
-test = open('../data/'+str(sys.argv[1])+'test.svm', 'w')
-train = open('../data/'+str(sys.argv[1])+'train.svm', 'w')
+test = open('../data/'+str(sys.argv[1])+'test'+str(i)+'.svm', 'w')
+train = open('../data/'+str(sys.argv[1])+'train'+str(i)+'.svm', 'w')
 F = open('../data/'+str(sys.argv[1])+'data.svm', 'r').readlines()
 
 length=len(F)/5
 
 f1=F[:length]
-f2=F[length+1:2*length]
-f3=F[2*length+1:3*length]
-f4=F[3*length+1:4*length]
-f5=F[4*length+1:5*length]
+f2=F[length:2*length]
+f3=F[2*length:3*length]
+f4=F[3*length:4*length]
+f5=F[4*length:5*length]
 
 F=[f1, f2, f3, f4, f5]
 
 print i
 print j
-test.write(F[i])
+
+for line in F[i]:
+	test.write('{0}'.format(line))
 test.close()
-for inidex in j:
-	train.write(F[index])
+for index in j:
+	for line in F[index]:
+		train.write('{0}'.format(line))
 train.close()
