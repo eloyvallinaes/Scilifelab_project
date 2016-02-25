ae74a9e5f6a41bb2e2b0e0e2b298d12a184bc057
Eloy <eloyvallina33@gmail.com>
2016-02-25 18:18:45 +0100

    New set division, new feature extraction

--- scripts/feat-extract.py
+++ scripts/feat-extract.py
@@ -21,26 +21,27 @@ code = []
 PC= []
 nametonumb = {'A':[1,3], 'R':[2,1], 'N':[3,2], 'D':[4,1], 'C':[5,2], 'Q':[6,2], 'E':[7,1], 'G':[8,3], 'H':[9,1], 'I':[10,3], 'L':[11,3], 'K':[12,1], 'M':[13,3], 'F':[14,3], 'P':[15,3], 'S':[16,2], 'T':[17,2], 'W':[18,2], 'Y':[19,2], 'V':[20,3]}
 
-f = open('../data/buried-exposed.3line').readlines()
+for set in ['set1', 'set2', 'set3', 'set4', 'set5', 'set6']:
+	f = open('../data/'+set+'.3line').readlines()
 
-for i in range(0, 375*3, 3):
-	labels.append(f[i].strip())
-	seqs.append(f[i+1].strip())
-	groups.append(f[i+2].strip())
+	for i in range(0, 375*3, 3):
+		labels.append(f[i].strip())
+		seqs.append(f[i+1].strip())
+		groups.append(f[i+2].strip())
 
-for seq in seqs:
-	for letter in seq:
-		code.append(nametonumb[letter][0])
-		PC.append(nametonumb[letter][1])
-for group in groups:
-	for letter in group:
-		if letter.strip() == 'b':
-			outcome.append(1)
-		else:
-			outcome.append(-1)
+	for seq in seqs:
+		for letter in seq:
+			code.append(nametonumb[letter][0])
+			PC.append(nametonumb[letter][1])
+	for group in groups:
+		for letter in group:
+			if letter.strip() == 'b':
+				outcome.append(1)
+			else:
+				outcome.append(-1)
 
-f = open('../data/'+str(sys.argv[1]+'data.svm'), 'w')
-for j in range(len(outcome)):
-	f.write ('{0} 1:{1} 2:{2} \n'.format(outcome[j], code[j], PC[j]))
+	f = open('../data/'+str(sys.argv[1]+set+'.svm'), 'w')
+	for j in range(len(outcome)):
+		f.write ('{0} 1:{1} 2:{2} \n'.format(outcome[j], code[j], PC[j]))
 
-f.close()
+	f.close()
