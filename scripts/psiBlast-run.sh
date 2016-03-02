dbadb1d016f04a867cb30df3db48f6c02f7c7581
Eloy <eloyvallina33@gmail.com>
2016-03-02 19:27:23 +0100

    Loop corrected so that one process is run at a time with cmd wait

--- scripts/psiBlast-run.sh
+++ scripts/psiBlast-run.sh
@@ -2,9 +2,10 @@
 
 # Run psiblast on each single sequence fasta file
 
-for sequence in $( ls ../data/multi_fasta/* )
+for sequence in $( ls ../data/multi_fasta/* | grep "\.fasta" )
 do
-name=${sequence%\.fasta}
-nohup blastpgp -j 3 -d ../data/uniref90.fasta -i $sequence \
-	-o "$name".blastpgp -Q "$name".psi &
+	name=${sequence%\.fasta}
+	nohup blastpgp -j 3 -d ../data/uniref90.fasta -i $sequence \
+		-o "$name".blastpgp -Q "$name".psi &
+	wait
 done
